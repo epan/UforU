834470684a8c812d5ff630df340a5a1a75755edb
bhattprateek <prateekbhatt94@gmail.com>
2017-05-17 19:36:53 -0700

    (fix)Fix spacing issue in header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return (
     <nav className="navbar navbar-inverse">
-    <div className="container-fluid">
-      <div className="navbar-header">
-        <a href={global.window.location.origin} className="navbar-brand">UforU</a>
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a href={global.window.location.origin} className="navbar-brand">UforU</a>
+        </div>
       </div>
-    </div>
     </nav >
   );
 };
