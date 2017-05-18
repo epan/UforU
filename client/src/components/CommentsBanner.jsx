d44b28f4322bdb197bfd2165593382a5d58af902
Alex Seiden <alex.seiden@gmail.com>
2017-05-18 14:20:10 -0700

    (feat) implement formatting in comment banner

--- client/src/components/CommentsBanner.jsx
+++ client/src/components/CommentsBanner.jsx
@@ -1,6 +1,16 @@
 const CommentsBanner = (props) => {
+  const college = props.college;
+
   return (
-    <p>This is the comments banner</p>
+      <div className = "card">
+        <div className="row">
+          <div className="col-md-3">
+            <img className="img-responsive cardImages style_prevu_kit" src = {college.image_url}/>
+          </div>
+          <a className="college-name" href={'http://' + college.website_url}> {college.name}</a>
+          <p className="description">{college.description}</p>
+        </div>
+      </div>
   );
 };
 
