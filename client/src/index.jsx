1725cee380d2bf14730e670e15cb7da0ce3ba8b7
Eric Pan <epan@users.noreply.github.com>
2017-05-19 14:31:34 -0700

    (feat) Connect client and server socket w a log

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory, DefaultRoute, IndexLink } from 'react-router';
+import io from 'socket.io-client';
 
 import Home from './components/Home.jsx';
 import Container from './components/Container.jsx';
 import Results from './components/Results.jsx';
 import Survey from './components/Survey.jsx';
 import CommentsPage from './components/CommentsPage.jsx';
+
+const socket = io.connect('http://127.0.0.1:3000');
+socket.on('connect', function(data) {
+  socket.emit('event', 'cash me outside');
+});
 
 class App extends React.Component {
   render() {
