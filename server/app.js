2dcd9bd065f0d024ad31af0c0f72c0c5030a1800
Eric Pan <epan@users.noreply.github.com>
2017-05-18 20:20:16 -0700

    (feat) Add HTTP req logger

--- server/app.js
+++ server/app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var path = require('path');
-var router = require('./router/router.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const logger = require('morgan');
+const router = require('./router/router.js');
 
-var app = express();
-var IP = process.env.IP || 'localhost';
-var PORT = process.env.PORT || 3000;
+const app = express();
+const IP = process.env.IP || 'localhost';
+const PORT = process.env.PORT || 3000;
 
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
