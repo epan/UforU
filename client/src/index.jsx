917444940ae5509caa4343c5d823afb04034052a
Eric Pan <epan@users.noreply.github.com>
2017-05-18 19:20:04 -0700

    (feat) Commenting to-dos

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -14,6 +14,7 @@ class App extends React.Component {
         <Route path='/' component={Container}>
           <IndexRoute component={Home} />
           <Route path='results' component={Results} />
+          {/* Add route to /college/:id */}
         </Route>
       </Router>
     );
