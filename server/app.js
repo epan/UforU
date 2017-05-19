33a44c6695f0c9da391c7f3f8e54ab7b0c504705
Eric Pan <epan@users.noreply.github.com>
2017-05-19 13:59:26 -0700

    (feat) Add socket.io to server

--- server/app.js
+++ server/app.js
@@ -1,25 +1,22 @@
-const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const path = require('path');
+const express = require('express');
 const logger = require('morgan');
-const router = require('./router/router.js');
+const path = require('path');
+const router = require('./router/router');
+const io = require('socket.io');
 
 const app = express();
-const IP = process.env.IP || 'localhost';
 const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, function () {
+  console.log('listening on', PORT);
+});
+
+io(server);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(router);
-
-app.listen(PORT, function () {
-  console.log('listening right now on port', PORT);
-});
-console.log('listening on', IP, PORT);
-
-module.exports.app = app;
