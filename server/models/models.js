3cb73b7694199a1988d10a63526ce94d81bac594
Eric Pan <epan@users.noreply.github.com>
2017-05-18 19:19:44 -0700

    (refactor) Simplify college query

--- server/models/models.js
+++ server/models/models.js
@@ -6,7 +6,7 @@ const querySchoolTable = require('./queryHelper.js').querySchoolTable;
 module.exports = {
   colleges: {
     get: function(cb) {
-      connection.query('SELECT name, address, state, description, admission_rate, tuition, size, average_gpa, average_sat_score, sports_division, website_url, image_url FROM Universities', function(err, results, fields) {
+      connection.query('SELECT * FROM Universities', function(err, results, fields) {
         if (err) {
           cb (err, null);
         } else {
